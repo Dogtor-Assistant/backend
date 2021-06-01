be9bfb983689304fa1821cc16659e8405ca19d78
Mathias Quintero <me@quintero.io>
2021-06-01 21:37:44 +0200

    Adding resolver snippet

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -3,7 +3,7 @@ import type { QueryResolvers } from '@resolvers';
 import type { Context } from 'context';
 
 const Query: QueryResolvers<Context> = {
-    greeting(_0, _1, context: Context) {
+    greeting(_0, _1, context) {
         if (context.authenticated != null) {
             return `Hello, User ${context.authenticated.id}`;
         }
