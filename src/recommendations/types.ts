2ec2feac43e7defc268f17548819e1fce769abb1
Mathias Quintero <me@quintero.io>
2021-06-03 21:29:19 +0200

    Adding basic types for recommendation system

---
+++ src/recommendations/types.ts
@@ -0,0 +1,31 @@
+
+export interface UserMedicalData {
+    dateOfBirth: Date
+    insurance: 'Public' | 'Private'
+    gender?: 'Male' | 'Female' | 'TransgenderMale' | 'TransgenderFemale' | 'NonBinary'
+    medications: string[]
+    conditions: string[]
+}
+
+interface BasicRecommendation {
+    service: string
+}
+
+type PeriodicRecommendation = BasicRecommendation & {
+    kind: 'periodic'
+    periodInDays: number
+}
+
+type OneTimeRecommendation = BasicRecommendation & {
+    kind: 'single'
+}
+
+export type Recommendation = PeriodicRecommendation | OneTimeRecommendation;
+
+export type InferenceRuleResult = 'Skip' | 'Show' | 'Deny'
+export type InferenceRule = ((data: UserMedicalData) => InferenceRuleResult) | InferenceRule[] | InferenceRuleResult
+
+export type RecommendationStrategy = {
+    recommendation: Recommendation
+    rule: InferenceRule
+} | RecommendationStrategy[] | Recommendation
