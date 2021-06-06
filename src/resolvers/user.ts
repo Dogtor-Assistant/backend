92dc9c6ad7fc7ef2f1681a0bcab4ca7cce0eb000
Mathias Quintero <me@quintero.io>
2021-06-06 09:57:24 +0200

    Fixing User Resolvers

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,10 +1,33 @@
 import type { UserResolvers } from '@resolvers';
 
+import Doctor from 'models/Doctor';
+import Patient from 'models/Patient';
 import { buildId } from 'utils/ids';
 
 const User: UserResolvers = {
-    id({ id }) {
+    async doctorProfile({ doctorRef }) {
+        if (doctorRef == null) {
+            return null;
+        }
+        return await Doctor.findById(doctorRef);
+    },
+    firstname({ firstName }) {
+        return firstName;
+    },
+    id({ _id: id }) {
+        if (id == null) {
+            throw 'Uninitialized Value!';
+        }
         return buildId('User', id);
+    },
+    lastname({ lastName }) {
+        return lastName;
+    },
+    async patientProfile({ patientRef }) {
+        if (patientRef == null) {
+            return null;
+        }
+        return await Patient.findById(patientRef);
     },
 };
 
