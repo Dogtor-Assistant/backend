ec5ec4fdeb7635904dcef115aa2d3e4467a44e8e
Mathias Quintero <me@quintero.io>
2021-06-06 09:57:09 +0200

    Fixing query resolver

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,6 +1,14 @@
 
 import type { QueryResolvers } from '@resolvers';
 
+import Appointment from 'models/Appointment';
+import Checkup from 'models/Checkup';
+import Doctor from 'models/Doctor';
+import Followup from 'models/Followup';
+import Patient from 'models/Patient';
+import Review from 'models/Review';
+import Service from 'models/Service';
+import User from 'models/User';
 import { deconstructId } from 'utils/ids';
 
 const Query: QueryResolvers = {
@@ -11,28 +19,39 @@ const Query: QueryResolvers = {
 
         return 'Hello World';
     },
-    me(_0, _1, { authenticated }) {
+    async me(_0, _1, { authenticated }) {
         if (authenticated == null) {
             return null;
         }
 
-        return {
-            __typename: 'User',
-            id: authenticated.id,
-        };
+        return await User.findOne({ _id : authenticated.id });
     },
-    node(_, { id: nodeId }) {
+    async node(_, { id: nodeId }) {
         const deconstructed = deconstructId(nodeId);
         if (deconstructed == null) {
             return null;
         }
 
         const [nodeType, id] = deconstructed;
-        // TODO: fetch from DB
-        return {
-            __typename: nodeType,
-            id,
-        };
+
+        switch (nodeType) {
+        case 'Appointment':
+            return await Appointment.findById(id);
+        case 'Checkup':
+            return await Checkup.findById(id);
+        case 'Doctor':
+            return await Doctor.findById(id);
+        case 'Followup':
+            return await Followup.findById(id);
+        case 'Patient':
+            return await Patient.findById(id);
+        case 'Review':
+            return await Review.findById(id);
+        case 'Service':
+            return await Service.findById(id);
+        case 'User':
+            return await User.findById(id);
+        }
     },
 };
 
